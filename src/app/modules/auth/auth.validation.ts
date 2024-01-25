3ec4944f16eea9dc0330023a5ca3b5c666cee011
Manik <tonysins43@gmail.com>
2024-01-25 10:34:04 +0600

    make validation shchema for user registration

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const userRegistrationValidationSchema = z.object({
+  fullName: z.string({
+    invalid_type_error: 'Full name must be string',
+    required_error: 'Full name is required',
+  }),
+  email: z.string({
+    invalid_type_error: 'Email must be string',
+    required_error: 'Email is required',
+  }),
+  password: z.string({
+    required_error: 'Password is required',
+  }),
+});
+
+export const authValidation = {
+  userRegistrationValidationSchema,
+};

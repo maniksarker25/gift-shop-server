6f2f65ee609a1b909490783da2a82705c242e997
Manik <tonysins43@gmail.com>
2024-01-25 14:07:53 +0600

    make password bcrypt

--- src/app/modules/auth/auth.model.ts
+++ src/app/modules/auth/auth.model.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { Schema, model } from 'mongoose';
 import { TUser } from './auth.interface';
-
+import bcrypt from 'bcrypt';
+import config from '../../config';
 const userSchema = new Schema<TUser>({
   fullName: {
     type: String,
@@ -16,6 +18,22 @@ const userSchema = new Schema<TUser>({
     type: String,
     required: true,
   },
+});
+
+// middleware for password hashed
+userSchema.pre('save', async function (next) {
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+//
+userSchema.post('save', function (doc, next) {
+  doc.set('password', undefined);
+  next();
 });
 
 export const User = model<TUser>('User', userSchema);

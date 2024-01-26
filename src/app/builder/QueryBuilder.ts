2ce3d16c3cb8ea6d96aef420e6ab09d130ed31c2
Manik <tonysins43@gmail.com>
2024-01-26 19:48:13 +0600

    make filter and search

---
+++ src/app/builder/QueryBuilder.ts
@@ -0,0 +1,31 @@
+import { FilterQuery, Query } from 'mongoose';
+
+class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+
+  search(searchableField: string) {
+    const searchTerm = this?.query?.searchTerm;
+    if (searchTerm) {
+      this.modelQuery = this?.modelQuery.find({
+        [searchableField]: { $regex: searchTerm, $options: 'i' },
+      } as FilterQuery<T>);
+    }
+    return this;
+  }
+
+  filter() {
+    const queryObj = { ...this.query };
+    const excludeFields = ['searchTerm'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
+    return this;
+  }
+}
+
+export default QueryBuilder;
--- src/app/modules/gift/gift.services.ts
+++ src/app/modules/gift/gift.services.ts
@@ -1,3 +1,4 @@
+import QueryBuilder from '../../builder/queryBuilder';
 import { TGift } from './gift.interface';
 import { Gift } from './gift.model';
 
@@ -6,12 +7,15 @@ const createGiftIntoDB = async (payload: TGift) => {
   return result;
 };
 
-// get gifts from db
-const getGiftsFromDB = async () => {
-  const result = await Gift.find();
+// get gifts from db--------------
+const getGiftsFromDB = async (query: Record<string, unknown>) => {
+  const giftQuery = new QueryBuilder(Gift.find(), query)
+    .search('name')
+    .filter();
+  const result = await giftQuery.modelQuery;
   return result;
 };
-// update gift from db
+// update gift from db ------------------
 const updateGiftFromDB = async (id: string, payload: Partial<TGift>) => {
   const result = await Gift.findByIdAndUpdate(id, payload, {
     new: true,
@@ -19,7 +23,7 @@ const updateGiftFromDB = async (id: string, payload: Partial<TGift>) => {
   });
   return result;
 };
-// delete gift from db
+// delete gift from db---------------------
 const deleteGiftFromDB = async (id: string) => {
   const result = await Gift.findByIdAndDelete(id);
   return result;

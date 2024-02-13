321e9440c0042d34d9176b775e1c8147da96322f
Manik <tonysins43@gmail.com>
2024-02-13 23:43:08 +0600

    some changes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import router from './app/routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 
-// parser
+// parser----------------------
 app.use(express.json());
 app.use(cors());
 

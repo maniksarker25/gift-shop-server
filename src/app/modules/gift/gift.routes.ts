b59a1c98d408be4fa0d80ee712289a82f442d85c
Manik <tonysins43@gmail.com>
2024-02-14 19:06:24 +0600

    add auth guard to create , update , delete gift route

--- src/app/modules/gift/gift.routes.ts
+++ src/app/modules/gift/gift.routes.ts
@@ -3,23 +3,32 @@ import auth from '../../middlewares/auth';
 import { giftControllers } from './gift.controllers';
 import validationRequest from '../../middlewares/validationRequest';
 import { giftValidation } from './gift.validation';
+import { USER_ROLE } from '../auth/auth.constant';
 
 const router = express.Router();
 
 router.post(
   '/',
-  auth(),
+  auth(USER_ROLE.manager),
   validationRequest(giftValidation.createGiftValidationSchema),
   giftControllers.createGift,
 );
-router.post('/delete-gifts', auth(), giftControllers.deleteMultipleGift);
-router.get('/', auth(), giftControllers.getGifts);
+router.post(
+  '/delete-gifts',
+  auth(USER_ROLE.manager),
+  giftControllers.deleteMultipleGift,
+);
+router.get('/', auth(USER_ROLE.manager), giftControllers.getGifts);
 router.put(
   '/:id',
-  auth(),
+  auth(USER_ROLE.manager),
   validationRequest(giftValidation.updateGiftValidationSchema),
   giftControllers.updateGift,
 );
-router.delete('/:id', auth(), giftControllers.deleteSingleGift);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.manager),
+  giftControllers.deleteSingleGift,
+);
 
 export const giftRoutes = router;
